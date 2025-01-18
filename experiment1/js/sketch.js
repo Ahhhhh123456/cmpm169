331aa8af6ad8ae0d76129897a6a681aa56db0251
Jason Li <Jason.Li8885551@gmail.com>
2025-01-18 15:28:12 -0800

    Fixing sketch.js

--- experiment1/js/sketch.js
+++ experiment1/js/sketch.js
@@ -32,32 +32,14 @@ class MyClass {
     }
 }
 
-function resizeScreen() {
-  centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
-  centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
-  console.log("Resizing...");
-  resizeCanvas(canvasContainer.width(), canvasContainer.height());
-  // redrawCanvas(); // Redraw everything based on new size
-}
 
 // setup() function is called once when the program starts
 function setup() {
+  createCanvas(800, 800);
   background(0);
   currentColor = color(255);
   noFill();
-  // place our canvas, making it fit our container
-  canvasContainer = $("#canvas-container");
-  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-  canvas.parent("canvas-container");
-  // resize canvas is the page is resized
 
-  // create an instance of the class
-  myInstance = new MyClass("VALUE1", "VALUE2");
-
-  $(window).resize(function() {
-    resizeScreen();
-  });
-  resizeScreen();
 }
 
 function draw() {
